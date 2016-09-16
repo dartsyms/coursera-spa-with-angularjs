c552e77ae205691c4e5fff3b46c02cf8c8313c9b
sanchez <dartsyms@gmail.com>
2016-09-16 17:43:23 +0400

    Assignment solution

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -1,3 +1,38 @@
 (function() {
   'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.countCheck = function() {
+      $scope.empty = true;
+      $scope.tooMuch = false;
+      if ($scope.items == undefined || $scope.items === '') {
+        $scope.message = "Please, enter the data first!";
+        return;
+      }
+      // empty elements are not counted as items
+      var count = 0;
+      $scope.items.split(',').forEach(function(element) {
+        if (element.trim() != '') {
+            count++;
+        }
+      });
+
+      if (count > 3) {
+        $scope.empty = false;
+        $scope.tooMuch = true;
+        $scope.message = "Too much!";
+      } else if (count == 0) {
+        $scope.empty = true;
+        $scope.message = "An empty is not an item";
+      } else {
+        $scope.empty = false;
+        $scope.message = "Enjoy!";
+      }
+    };
+  }
 })();

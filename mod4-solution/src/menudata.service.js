80a4572dac85d83c2c19a61305775f9fc8b74054
sanchez <dartsyms@gmail.com>
2016-10-08 17:04:07 +0400

    Fixed error unreferenced variable in service

--- mod4-solution/src/menudata.service.js
+++ mod4-solution/src/menudata.service.js
@@ -15,19 +15,19 @@
       }).then(function (response) {
         return response.data;
       });
-    }
-  };
+    };
 
-  svc.getItemsForCategory = function (catShortName) {
-    return $http({
-      method: 'GET',
-      url: (ApiBasePath + '/menu_items.json'),
-      params: {
-        category: catShortName
-      }
-    }).then(function (response) {
-      return response.data;
-    });
+    svc.getItemsForCategory = function (catShortName) {
+      return $http({
+        method: 'GET',
+        url: (ApiBasePath + '/menu_items.json'),
+        params: {
+          category: catShortName
+        }
+      }).then(function (response) {
+        return response.data;
+      });
+    };
   };
 
 })();
